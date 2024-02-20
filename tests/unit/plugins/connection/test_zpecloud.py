0659251843499f6af130dca15de98524a892545a
Daniel Nesvera <daniel.nesvera@zpesystems.com>
2024-02-20 13:54:50 +0000

    add log functions

--- tests/unit/plugins/connection/test_zpecloud.py
+++ tests/unit/plugins/connection/test_zpecloud.py
@@ -5,3 +5,48 @@ import os
 import pytest
 from unittest.mock import MagicMock
 from unittest.mock import patch
+
+
+#### Overwritten methods
+
+# _connect
+# exec_command
+# put_file
+# fetch_file
+# close
+# reset
+
+
+#### Other methods
+
+# _create_api_session
+# _wrapper_exec_command
+# _create_profile
+# _delete_profile
+# _apply_profile
+# _wait_job_to_finish
+# _process_put_file
+# _process_fetch_file
+#_wrapper_put_file
+#_wrapper_fetch_file
+
+
+
+def test_missing_credentials():
+    pass
+
+def test_get_credentials_from_variables():
+    pass
+
+def test_get_credentials_from_env():
+    pass
+
+def test_run_task_offline_device_fails():
+    pass
+
+def test_wait_job_finish_timeout():
+    pass
+
+def test_wait_job_finish_succeed():
+    pass
+
